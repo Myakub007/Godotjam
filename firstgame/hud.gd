467ac147b44f097c0490a920230e4dc9c1d0cb4a
Md Yakoob <md100yakub@gmail.com>
2025-03-22 22:11:07 +0530

    Game present in game folder

--- firstgame/hud.gd
+++ firstgame/hud.gd
@@ -28,5 +28,3 @@ func _on_start_button_pressed():
 	
 func _on_message_timer_timeout():
 	$Message.hide()
-	
-	
--- firstgame/player.gd
+++ firstgame/player.gd
@@ -41,7 +41,7 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hide()
 	hit.emit()
 	# below line deffered -> disable collision so that only 1 hit is taken at a time
